11800265761adc39db79aa75fa187b3c280c6ce0
YuanyaTianchi <YuanyaTianchi@users.noreply.github.com>
2023-11-26 11:20:29 +0000

    deploy: 7823bde548b1908efd3582f46550c38805c7cf25

--- 戏/命运方舟.go
+++ 戏/命运方舟.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 const (
 	攻增  增强类型 = "攻增"
@@ -12,6 +14,11 @@ const (
 	蓄伤增 增强类型 = "蓄伤增"
 
 	攻增2 增强类型 = "攻增2"
+
+	背击增  增强类型 = "背击增"
+	噩梦法增 增强类型 = "噩梦法增"
+	支配技增 增强类型 = "支配技增"
+	支配时增 增强类型 = "支配时增"
 
 	default率 = 1
 	default系 = 0
@@ -26,6 +33,9 @@ var (
 	}}
 	重量强化3 = 刻印{名字: "重量强化3", 增强集: []增强体{
 		{类型: 攻增, 率: default率, 系: 0.18},
+	}}
+	以太充能3 = 刻印{名字: "以太充能3", 增强集: []增强体{
+		{类型: 攻增, 率: default率, 系: 0.15},
 	}}
 	肾上腺素3 = 刻印{名字: "肾上腺素3", 增强集: []增强体{
 		{类型: 攻增, 率: default率, 系: 0.05},
@@ -42,13 +52,29 @@ var (
 		{类型: 首伤增, 率: default率, 系: 0.2},
 	}}
 	奇袭大师3 = 刻印{名字: "奇袭大师3", 增强集: []增强体{
-		{类型: 伤量增, 率: default率, 系: 0.25},
+		{类型: 伤量增, 率: 1, 系: 0.25},
 	}}
 	蓄力强化3 = 刻印{名字: "蓄力强化3", 增强集: []增强体{
 		{类型: 蓄伤增, 率: default率, 系: 0.2},
 	}}
 	迅捷利刃3 = 刻印{名字: "迅捷利刃3", 增强集: []增强体{
 		{类型: 额伤量, 率: default率, 系: 0.18},
+	}}
+
+	背击 = 刻印{名字: "背击", 增强集: []增强体{
+		{类型: 背击增, 率: 1, 系: 0.05},
+	}}
+
+	噩梦2支配2绝地2 = 刻印{名字: "噩梦2+支配2+绝地2", 增强集: []增强体{
+		{类型: 噩梦法增, 率: default率, 系: 0.12},
+		{类型: 支配技增, 率: default率, 系: 0.10},
+		{类型: 支配时增, 率: default率, 系: 0.10},
+		{类型: 暴增, 率: 0, 系: 0.72},
+	}}
+	噩梦2支配4 = 刻印{名字: "噩梦2+支配4", 增强集: []增强体{
+		{类型: 噩梦法增, 率: default率, 系: 0.12},
+		{类型: 支配技增, 率: default率, 系: 0.25},
+		{类型: 支配时增, 率: default率, 系: 0.10},
 	}}
 )
 
@@ -134,6 +160,7 @@ func DamagePrintWithOptions(暴击递增率 float64, 刻印集 ...刻印) {
 			break
 		}
 	}
+	fmt.Println("-----by 天竾-----")
 }
 
 func main() {
@@ -141,25 +168,45 @@ func main() {
 	// DamagePrintWithOptions(0.2, 迅捷利刃3, 重量强化3, 肾上腺素3, 尖刺重锤3)
 	// DamagePrintWithOptions(0.2, 怨恨3, 重量强化3, 肾上腺素3, 尖刺重锤3)
 
-	fmt.Println("国服下版本刀锋计算：")
-	fmt.Println("基础爆伤增益1.55（头背套）")
-	fmt.Println("500会心17.89暴击率，头背套17暴击率，背击10暴击，即其它暴击率44.89左右（按40算，会心不一定能吃到500），但是打本可能会有辅助")
-	DamagePrintWithOptions(0.2, 刀锋残影3, 肾上腺素3, 奇袭大师3, 蓄力强化3)
-	DamagePrintWithOptions(0.2, 刀锋残影3, 肾上腺素3, 奇袭大师3, 蓄力强化3, 精密短刀3)
-	DamagePrintWithOptions(0.2, 刀锋残影3, 肾上腺素3, 奇袭大师3, 蓄力强化3, 尖刺重锤3)
-	DamagePrintWithOptions(0.2, 刀锋残影3, 肾上腺素3, 奇袭大师3, 蓄力强化3, 咒术人偶3)
-	DamagePrintWithOptions(0.2, 刀锋残影3, 肾上腺素3, 奇袭大师3, 蓄力强化3, 怨恨3)
-	fmt.Println("精密肾上一起带，其它暴击高了以后暴击溢出，上限较低；实测刀锋残影攻击力与人偶攻击力非加算")
-	fmt.Println("无溢出风险性价比选择精密短刀（很容易溢出）；有溢出风险选择尖刺重锤3（后续爆伤较高时会弱于人偶）；只考虑伤害无脑怨恨")
-	fmt.Println("by Chihilis/天堇晴雨瑶桃竾")
-	DamagePrintWithOptions(0.2, 刀锋残影3, 奇袭大师3, 蓄力强化3, 精密短刀3, 尖刺重锤3)
-	DamagePrintWithOptions(0.2, 刀锋残影3, 奇袭大师3, 蓄力强化3, 怨恨3, 精密短刀3)
-	DamagePrintWithOptions(0.2, 刀锋残影3, 奇袭大师3, 蓄力强化3, 怨恨3, 尖刺重锤3)
-	fmt.Println("精密短刀3暴击率越低收益越高, 尖刺重锤3暴击率越高收益越高 两个一起带尖刺重锤会使收益变得更正向")
+	/*
+		fmt.Println("国服下版本刀锋计算：")
+		fmt.Println("基础爆伤增益1.55（头背套）")
+		fmt.Println("500会心17.89暴击率，头背套17暴击率，背击10暴击，即其它暴击率44.89左右（按40算，会心不一定能吃到500），但是打本可能会有辅助")
+		DamagePrintWithOptions(0.2, 刀锋残影3, 肾上腺素3, 奇袭大师3, 蓄力强化3)
+		DamagePrintWithOptions(0.2, 刀锋残影3, 肾上腺素3, 奇袭大师3, 蓄力强化3, 精密短刀3)
+		DamagePrintWithOptions(0.2, 刀锋残影3, 肾上腺素3, 奇袭大师3, 蓄力强化3, 尖刺重锤3)
+		DamagePrintWithOptions(0.2, 刀锋残影3, 肾上腺素3, 奇袭大师3, 蓄力强化3, 咒术人偶3)
+		DamagePrintWithOptions(0.2, 刀锋残影3, 肾上腺素3, 奇袭大师3, 蓄力强化3, 怨恨3)
+		fmt.Println("精密肾上一起带，其它暴击高了以后暴击溢出，上限较低；实测刀锋残影攻击力与人偶攻击力非加算")
+		fmt.Println("无溢出风险性价比选择精密短刀（很容易溢出）；有溢出风险选择尖刺重锤3（后续爆伤较高时会弱于人偶）；只考虑伤害无脑怨恨")
+		fmt.Println("by Chihilis/天堇晴雨瑶桃竾")
+		DamagePrintWithOptions(0.2, 刀锋残影3, 奇袭大师3, 蓄力强化3, 精密短刀3, 尖刺重锤3)
+		DamagePrintWithOptions(0.2, 刀锋残影3, 奇袭大师3, 蓄力强化3, 怨恨3, 精密短刀3)
+		DamagePrintWithOptions(0.2, 刀锋残影3, 奇袭大师3, 蓄力强化3, 怨恨3, 尖刺重锤3)
+		fmt.Println("精密短刀3暴击率越低收益越高, 尖刺重锤3暴击率越高收益越高 两个一起带尖刺重锤会使收益变得更正向")
+	*/
+
+	// DamagePrintWithOptions(0.45, 噩梦2支配4, 重量强化3, 迅捷利刃3, 尖刺重锤3, 怨恨3)
+	// DamagePrintWithOptions(0.45, 噩梦2支配2绝地2, 重量强化3, 迅捷利刃3, 奇袭大师3, 怨恨3)
+
+	// // 迅捷减cd和觉醒刻印减cd乘算
+	// fmt.Println("迅捷-31，觉醒套-18", float64(300)*OS(0.31)*OS(0.18))
+	// fmt.Println("迅捷-31，觉醒套-18 + 主宰-15：", float64(300)*OS(0.31)*OS(0.18+0.15))
+	// fmt.Println("迅捷-31，觉醒套-18, 觉醒2-25：", float64(300)*OS(0.31)*OS(0.18)*OS(0.25))
+	// fmt.Println("迅捷-31，觉醒套-18 + 主宰-15, 觉醒1-10：", float64(300)*OS(0.31)*OS(0.18+0.15)*OS(0.1))
+	// fmt.Println("迅捷-31，觉醒套-18 + 主宰-15, 觉醒2-25：", float64(300)*OS(0.31)*OS(0.18+0.15)*OS(0.25))
+
+	// DamagePrintWithOptions(0.2, 肾上腺素3, 怨恨3, 重量强化3, 尖刺重锤3)
+	// DamagePrintWithOptions(0.2, 迅捷利刃3, 怨恨3, 重量强化3, 尖刺重锤3)
+	// DamagePrintWithOptions(0.2, 肾上腺素3, 奇袭大师3, 重量强化3, 尖刺重锤3)
+	// DamagePrintWithOptions(0.2, 肾上腺素3, 奇袭大师3, 重量强化3, 迅捷利刃3)
+	// DamagePrintWithOptions(0.2, 迅捷利刃3, 奇袭大师3, 重量强化3, 尖刺重锤3)
+	// DamagePrintWithOptions(0.2, 怨恨3, 奇袭大师3, 重量强化3, 尖刺重锤3)
 
 	// DamagePrintWithOptions(0.2, 怨恨3, 奇袭大师3, 肾上腺素3, 咒术人偶3)
 
 	// DamagePrintWithOptions(0.2, 怨恨3, 奇袭大师3, 肾上腺素3, 咒术人偶3)
+
 	// DamagePrintWithOptions(0.2, 怨恨3, 精密短刀3, 肾上腺素3, 咒术人偶3)
 	// DamagePrintWithOptions(0.2, 迅捷利刃3, 重量强化3, 肾上腺素3, 尖刺重锤3)
 	// DamagePrintWithOptions(0.2, 迅捷利刃3, 重量强化3, 肾上腺素3, 尖刺重锤3)
@@ -168,6 +215,17 @@ func main() {
 	// DamagePrintWithOptions(0.2, 怨恨3, 重量强化3, 精密短刀3, 尖刺重锤3)
 	// DamagePrintWithOptions(0.2, 怨恨3, 精密短刀3, 肾上腺素3, 尖刺重锤3)
 	// DamagePrintWithOptions(0.2, 精密短刀3, 重量强化3, 肾上腺素3, 尖刺重锤3)
+
+	DamagePrintWithOptions(0.2, 尖刺重锤3)
+	DamagePrintWithOptions(0.2, 精密短刀3)
+	DamagePrintWithOptions(0.2, 重量强化3)
+	DamagePrintWithOptions(0.2, 精密短刀3, 尖刺重锤3)
+	DamagePrintWithOptions(0.2, 重量强化3, 尖刺重锤3)
+	DamagePrintWithOptions(0.2, 重量强化3, 怨恨3)
+}
+
+func OS(num float64) float64 {
+	return float64(1) - num
 }
 
 //3000000*1.64 =4920000
